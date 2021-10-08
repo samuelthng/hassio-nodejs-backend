fc0fdc3bc5d9db2610cd0b0a59cac1a4f81e082a
Samuel Thng <samuel@thng.sg>
2021-10-08 23:56:23 +0800

    Added: Check for existing repository.

--- hassio-nodejs-addon/run.sh
+++ hassio-nodejs-addon/run.sh
@@ -4,13 +4,36 @@ node -v
 npm -v
 git --version
 
+clone_repository() {
+	bashio::log.info "Cloning $REPOSITORY…"
+	git clone $REPOSITORY $REPO_DIR
+}
+
+REPO_DIR='/data/repository'
 REPOSITORY="$(bashio::config 'repository')"
-bashio::log.info "Cloning $REPOSITORY…"
-git clone $REPOSITORY /data/repository
-cd /data/repository
+if [[ -d $REPO_DIR ]]
+then
+	cd $REPO_DIR
+	$CURRENT_REMOTE="$(git config --get remote.origin.url)"
+	if [[ $CURRENT_REMOTE == *$REPOSITORY* ]]
+	then
+		bashio::log.info "Repository already exists, pulling latest…"
+		git fetch
+		git pull
+	else
+		bashio::log.info "Repository has changed, purging directory…"
+		cd /data
+		rm -rf $REPO_DIR
+		clone_repository()
+	fi
+else
+	clone_repository()
+fi
 
 bashio::log.info "Installing Packages…"
+cd $REPO_DIR
 npm install
 
 bashio::log.info "Starting Application…"
+cd $REPO_DIR
 npm run start
