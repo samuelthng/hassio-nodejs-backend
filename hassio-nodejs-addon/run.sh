f41d2b506c10b15ada0b47849685967672374454
Samuel Thng <samuel@thng.sg>
2021-10-08 23:21:17 +0800

    Refactor: Moved repository cloning and package install into Dockerfile.

--- hassio-nodejs-addon/run.sh
+++ hassio-nodejs-addon/run.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/with-contenv bashio
 set +u
-REPOSITORY="$(bashio::config 'repository')"
-bashio::log.info "Repository: $REPOSITORY"
-bashio::log.info "Cloning…"
-git clone $REPOSITORY /repository
-
-bashio::log.info "Installing Packages…"
-cd /repository
-npm install
-
+node -v
+npm -v
 bashio::log.info "Starting Application…"
 npm run start
