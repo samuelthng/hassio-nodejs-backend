fffeb848a4df3879a20021b872915fdb622f791c
Samuel Thng <samuel@thng.sg>
2021-10-08 23:32:41 +0800

    Revert: Hassio doesn't load config before it builds the docker image.

--- hassio-nodejs-addon/run.sh
+++ hassio-nodejs-addon/run.sh
@@ -2,5 +2,15 @@
 set +u
 node -v
 npm -v
+git --version
+
+REPOSITORY="$(bashio::config 'repository')"
+bashio::log.info "Cloning $REPOSITORY…"
+git clone $REPOSITORY /repository
+
+bashio::log.info "Installing Packages…"
+cd /repository
+npm install
+
 bashio::log.info "Starting Application…"
 npm run start
